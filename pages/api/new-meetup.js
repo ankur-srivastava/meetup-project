82603d2ec0af5cd3ab6168569fa5b0538ef50ca9
ankur-srivastava <ankurlkw@gmail.com>
2022-06-06 07:08:06 +0530

    api to add meetup

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,24 @@
+import { MongoClient } from 'mongodb'
+
+const MONGODB_URL = 'mongodb+srv://test:2J9D7lIGt1eHHJBM@cluster0.jrdex.mongodb.net/meetups?retryWrites=true&w=majority'
+
 // POST API route - /api/new-meetup
-const handler = (req, res) => {
+const handler = async (req, res) => {
     const data = req.body
-    const { title, description, image, address } = data
-    
+
+    try {
+        const client = await MongoClient.connect(MONGODB_URL)
+        const db = client.db()
+
+        const meetups = db.collection('meetup')
+        await meetups.insertOne(data)
+        client.close()
+    } catch (e) {
+        console.log(e)
+    }
+    res.status(201).json({
+        message: 'Meetup Added'
+    })
 }
 
 export default handler
